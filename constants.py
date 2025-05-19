7f4cf2eabd6f0f3d864e543e774b936e2749ac5f
Diego Alvarez <diegoalvarezmore@gmail.com>
2025-05-19 01:47:47 -0500

    refactor: separate constants

---
+++ constants.py
@@ -0,0 +1,36 @@
+COLORS_RGB = {
+    "red": (255, 0, 0),
+    "green": (0, 128, 0),
+    "blue": (0, 0, 255),
+    "black": (0, 0, 0),
+    "white": (255, 255, 255),
+    "gray": (128, 128, 128),
+    "silver": (192, 192, 192),
+    "maroon": (128, 0, 0),
+    "purple": (128, 0, 128),
+    "fuchsia": (255, 0, 255),
+    "lime": (0, 255, 0),
+    "olive": (128, 128, 0),
+    "yellow": (255, 255, 0),
+    "navy": (0, 0, 128),
+    "teal": (0, 128, 128),
+    "aqua": (0, 255, 255),
+    "orange": (255, 165, 0),
+    "brown": (165, 42, 42),
+    "pink": (255, 192, 203),
+    "gold": (255, 215, 0)
+}
+
+CAR_TYPES = {
+    "compact": ["compact", "small car", "compact car", "hatchback"],
+    "sedan": ["sedan", "saloon", "4-door", "four-door"],
+    "suv": ["suv", "sport utility vehicle", "crossover", "crossover suv", "cuv"],
+    "truck": ["truck", "pickup", "pickup truck"],
+    "coupe": ["coupe", "2-door", "two-door", "sports car"],
+    "minivan": ["minivan", "van", "mpv", "multi-purpose vehicle"],
+    "convertible": ["convertible", "cabriolet", "roadster", "open top"],
+    "wagon": ["wagon", "estate car", "station wagon"],
+    "luxury": ["luxury", "premium", "executive"],
+    "electric": ["electric", "ev", "bev", "battery electric vehicle"],
+    "hybrid": ["hybrid", "phev", "plug-in hybrid", "mild hybrid"]
+}
